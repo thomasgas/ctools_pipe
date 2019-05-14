869f1a277c033c1b5d48f261b3d17fa360fce98f
Thomas Gasparetto <thomas.gasparetto@ts.infn.it>
2019-05-14 15:50:09 +0200

    irf handling should be working now. 3 prods implemented.

--- background_sim.py
+++ background_sim.py
@@ -4,6 +4,7 @@ import cscripts
 import sys
 import os
 import yaml
+from irf_handler import IRFPicker
 
 from astropy.io.misc import yaml as yl_as
 import astropy.units as u
@@ -11,18 +12,11 @@ import astropy.units as u
 
 def simulate_background(input_yaml):
     config_in = yaml.safe_load(open(input_yaml))
-    print(yaml.dump(config_in))
+    # print(yaml.dump(config_in))
 
-    irf_info = config_in['IRF']
-
-    # zenith = irf_info['zenith']
-    # prod_info = irf_info['prod']
-    # prod_version = prod_info['version']
-    # prod_number = prod_info['number']
-    # irf_site = irf_info['site']
-    # irf_time = u.Quantity(irf_info['time'])
-    # irf_pointing = irf_info['pointing']
-    # irf_subarray = irf_info['subarray']
+    irf = IRFPicker(config_in)
+    name_irf = irf.irf_pick()
+    print(name_irf)
 
     # parse the IRF input to get a list of IRFs compliant
     # with the one present in the ctools folders
