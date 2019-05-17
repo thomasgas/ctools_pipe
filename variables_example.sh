5cb7646c49340d018b0386a68b05f0a36b9836f7
Thomas Gasparetto <thomas.gasparetto@ts.infn.it>
2019-05-17 09:57:44 +0000

    use external environmental variables for MUST

---
+++ variables_example.sh
@@ -0,0 +1,7 @@
+#this script will be sourced by the script itsels
+export CONDA_PATH=/lapp_data/cta/gasparetto/miniconda3
+export CTOOLS_PIPE=/lapp_data/cta/gasparetto/ctools_stuff/ctools_pipe
+export CALDB=/lapp_data/cta/gasparetto/miniconda3/envs/ctools_p3/share/caldb
+
+# background simulation
+export BACK_FOLDER=/lapp_data/cta/gasparetto/ctools_stuff/bask_sim
