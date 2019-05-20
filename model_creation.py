d385a8125d08e8cadb54ad2e987fd12fce890c76
Thomas Gasparetto <thomas.gasparetto@ts.infn.it>
2019-05-20 12:12:15 +0200

    First implementation of model creation pipeline.

---
+++ model_creation.py
@@ -0,0 +1,137 @@
+from astropy.io import fits
+import numpy as np
+import astropy.units as u
+import os
+import yaml
+import glob
+import subprocess
+
+
+def create_txt(input_yaml):
+    """
+    Create fits and txt files for the generation of time variable spectra
+    :param input_yaml: YAML configuration file
+    :return: None (data saved to disk)
+    """
+    config_in = yaml.safe_load(open(input_yaml))
+
+    models = config_in['models']
+    input_data = models['input_data']['path']
+    output_data = models['output_data']
+
+    model_folder = f"{output_data}/models"
+    try:
+        os.mkdir(model_folder)
+    except:
+        print(f'{model_folder} already created')
+
+    list_files = glob.glob(f"{input_data}/*fits")
+
+    # create example fits
+    time_vals = np.zeros(4)
+    norm_vals = np.array([0, 1, 1, 0])
+
+    time_fits = fits.Column(name='TIME', array=time_vals, format='1D')
+    norm_fits = fits.Column(name='NORM', array=norm_vals, format='1D')
+    output = fits.BinTableHDU.from_columns([time_fits, norm_fits])
+
+    hdr = fits.Header()
+    output.header['EXTNAME'] = 'Time Profile'
+    output.header.comments['EXTNAME'] = "Name of this extension."
+    output.header['MJDREFI'] = 51544
+    output.header.comments['MJDREFI'] = "[days] Integer part of time reference MJD."
+    output.header['MJDREFF'] = 0.5
+    output.header.comments['MJDREFF'] = "[days] Fractional part of time reference MJD."
+    output.header['TIMEUNIT'] = 's'
+    output.header.comments['TIMEUNIT'] = "Time units."
+    output.header['TIMESYS'] = 'TT'
+    output.header.comments['TIMESYS'] = "Time system."
+    output.header['TIMEREF'] = 'LOCAL'
+    output.header.comments['TIMEREF'] = "Time reference."
+    output.header['TUNIT1'] = 's'
+
+    # output.writeto("banana.fits")
+    primary_hdu = fits.PrimaryHDU(header=hdr)
+    hdul = fits.HDUList([primary_hdu, output])
+
+    for file_name in list_files:
+        grb_name = file_name.split('/')[-1][:-5]
+        grb_test = fits.open(f"{file_name}")
+        try:
+            os.mkdir(f'{model_folder}/{grb_name}')
+        except:
+            print(f'{grb_name} already created')
+
+        try:
+            os.mkdir(f'{model_folder}/{grb_name}/spectra')
+            os.mkdir(f'{model_folder}/{grb_name}/lightcv')
+        except:
+            print(f'spectra and lightcv already created')
+
+        header_prim = grb_test['PRIMARY'].header
+
+        header_en = grb_test['ENERGIES'].header
+
+        # energies (with proper units)
+        energy_unit_fits = u.Unit(header_en['TUNIT1'])
+        energy_unit_ctools = u.MeV
+        energies = u.Quantity(
+            grb_test['ENERGIES'].data['Energies'],
+            energy_unit_fits
+        ).to_value(energy_unit_ctools)
+
+        # times (with proper units)
+        header_time = grb_test['TIMES'].header
+        time_unit_fits = u.Unit(header_time['TUNIT1'])
+        time_unit_ctools = u.s
+        times = u.Quantity(
+            grb_test['TIMES'].data['Times'],
+            time_unit_fits
+        ).to_value(time_unit_ctools)
+
+        # spectra: try to use EBL absorbed spectra (GRB-like)...otherwise use unabsorbed (more GW-like)
+        # Units will be added during the loop otherwise it's too slow
+        try:
+            header_spec = grb_test['EBL-ABS. SPECTRA'].header
+            spectra = grb_test['EBL-ABS. SPECTRA'].data
+        except:
+            header_spec = grb_test['SPECTRA'].header
+            spectra = grb_test['SPECTRA'].data
+
+        flux_unit_fits = u.Unit(header_spec['UNITS'])
+        flux_unit_ctools = u.Unit('ph / (MeV * s *cm^2)')
+
+        model_title = f"{model_folder}/{grb_name}/model_{grb_name}.txt"
+        TS = 1
+        RA = 0
+        DEC = 0
+        with open(model_title, 'w') as file:
+            for counter, (time_in, time_out) in enumerate(zip(times[:-1], times[1:])):
+                fits_name = f"{model_folder}/{grb_name}/lightcv/lc_{str(counter).zfill(3)}_tin-{time_in:.2f}_tend-{time_out:.2f}.fits"
+                spec_name = f"{model_folder}/{grb_name}/spectra/spec_{str(counter).zfill(3)}_tin-{time_in:.2f}_tend-{time_out:.2f}.txt"
+
+                save_spectra = u.Quantity(spectra.field(counter), flux_unit_fits).to_value(flux_unit_ctools)
+                np.savetxt(spec_name, np.c_[energies, save_spectra.field(counter)], newline="\n")
+
+                deltat = time_out - time_in
+
+                # empirical way to create a square signal according to the delta,
+                # which varies between one bin and the other
+                delta_scale = 1000.
+                fits_time = [time_in - deltat / (2 * delta_scale),
+                             time_in,
+                             time_out,
+                             time_out + deltat / (2 * delta_scale)]
+
+                output.data['TIME'] = np.array(fits_time)
+                output.writeto(fits_name, overwrite=True)
+
+                source = f"{grb_name}_{counter} Point  {TS}  {RA} {DEC} 0 0 0 FUNC 1.0  {spec_name} 1.0 {fits_name} \n"
+                file.write(source)
+
+        subprocess.Popen([
+            "python",
+            "/home/thomas/Programs/astro/sexten_2017/model_creator/scriptModel_variable.py",
+            model_title]
+        )
+
