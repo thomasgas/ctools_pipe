01db1a65658a0a3304ed8fbd439fb6fd4a5caf79
Thomas Gasparetto <thomas.gasparetto@ts.infn.it>
2019-07-13 19:28:19 +0200

    LAPP works now.

---
+++ launch_multiple.sh
@@ -0,0 +1,3 @@
+qsub -V -j oe -o out_err -q flash -m aeb /lapp_data/cta/gasparetto/ctools_stuff/GRB_PAPER/back_sim/tmp/scripts/launch_000.sh
+qsub -V -j oe -o out_err -q flash -m aeb /lapp_data/cta/gasparetto/ctools_stuff/GRB_PAPER/back_sim/tmp/scripts/launch_001.sh
+qsub -V -j oe -o out_err -q flash -m aeb /lapp_data/cta/gasparetto/ctools_stuff/GRB_PAPER/back_sim/tmp/scripts/launch_002.sh
---
+++ variables_LAPP.sh
@@ -0,0 +1,6 @@
+export MAIN_FOLDER=/lapp_data/cta/gasparetto/ctools_stuff/GRB_PAPER
+
+export CONDA_PATH=/lapp_data/cta/gasparetto/miniconda3
+export CTOOLS_PIPE=/lapp_data/cta/gasparetto/ctools_stuff/ctools_pipe
+export CALDB=/lapp_data/cta/gasparetto/miniconda3/envs/ctools_p3/share/caldb
+export PYTHON_EGG_CACHE=/lapp_data/cta/gasparetto/python_cache
