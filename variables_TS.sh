7ef031a065bc3ab88799b2bb54e6b76185559eff
Thomas Gasparetto <thomas.gasparetto@ts.infn.it>
2019-08-12 13:59:14 +0200

    add variables file for Trieste farm

---
+++ variables_TS.sh
@@ -0,0 +1,3 @@
+export MAIN_FOLDER=/gpfs/glast/Users/tgaspar/Dottorato/ASTRO/ANALISI/GRB_PAPER
+export CTOOLS_PIPE=/gpfs/glast/Users/tgaspar/Dottorato/ASTRO/ctools_pipe
+export ENV_PATH=/gpfs/glast/Users/tgaspar/software/anaconda3/envs/ctools_p3
